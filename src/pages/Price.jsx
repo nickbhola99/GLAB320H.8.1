ccad1e21d02ccdcfa8b84b22bbeb291941536a4b
nickbhola99 <bholaphillipn@gmail.com>
2024-09-27 12:08:43 -0400

    first commit

---
+++ src/pages/Price.jsx
@@ -0,0 +1,42 @@
+import {useState, useEffect} from "react";
+import {useParams} from "react-router-dom"
+
+export default function Price (props) {
+  const apiKey = import.meta.env.VITE_API_KEY;
+  const params = useParams()
+  const symbol = params.symbol
+  const url = `http://rest.coinapi.io/v1/exchangerate/${symbol}/USD?apikey=${apiKey}`;
+
+  const [coin, setCoin] = useState("null");
+
+  const getCoin = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setCoin(data);
+    } catch(e) {
+      console.error(e)
+    }
+  };
+
+  useEffect(() => {
+    getCoin();
+  }, []);
+
+  const loaded = () => {
+    return (
+      <div>
+        <h1>
+          {coin.asset_id_base}/{coin.asset_id_quote}
+        </h1>
+        <h2>{coin.rate}</h2>
+      </div>
+    );
+  };
+
+  const loading = () => {
+    return <h1>Loading...</h1>;
+  };
+
+  return coin && coin.rate ? loaded() : loading();
+}
